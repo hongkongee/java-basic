3bbecb40484e026d3fd56b02ae1349649a70b83e
jeongwon <tjtkdvl@gmail.com>
2024-02-27 11:01:15 +0900

    # Java oop (객체배열 문제)

--- src/oop/obj_arr/Score.java
+++ src/oop/obj_arr/Score.java
@@ -11,4 +11,107 @@ public class Score {
 
      - 캡슐화를 구현해서 작성해 주세요. (생성자는 맘대로 하세요.)
     */
+
+    private String name;
+    private int korean = -1;
+    private int english = -1;
+    private int math = -1;
+    private int total = 0;
+    private double average = 0;
+
+    public Score() {}
+
+    public Score(String name, int korean, int english, int math) {
+        this.name = name;
+        if(korean > 100 || korean < 0) {
+            System.out.println("잘못된 점수 입력입니다.");
+            return;
+        }
+        if(english > 100 || english < 0) {
+            System.out.println("잘못된 점수 입력입니다.");
+            return;
+        }
+        if(math > 100 || math < 0) {
+            System.out.println("잘못된 점수 입력입니다.");
+            return;
+        }
+
+
+        this.korean = korean;
+        this.english = english;
+        this.math = math;
+    }
+
+    // getter(), setter()
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setKorean(int korean) {
+        this.korean = korean;
+    }
+
+    public void setEnglish(int english) {
+        this.english = english;
+    }
+
+    public void setMath(int math) {
+        this.math = math;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getKorean() {
+        return korean;
+    }
+
+    public int getEnglish() {
+        return english;
+    }
+
+    public int getMath() {
+        return math;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public double getAverage() {
+        return average;
+    }
+
+    private void calcTotal() {
+        if(this.korean == -1 || this.english == -1 || this.math == -1) {
+            System.out.println("점수가 누락되어 있어 총점을 계산하지 못합니다.");
+            return;
+        }
+        this.total = this.korean + this.english + this.math;
+    }
+
+    private void calcAverage() {
+        if(this.total == 0) {
+            System.out.println("총점이 누락되어 있어 평균을 계산하지 못합니다.");
+            return;
+        }
+        this.average = this.total / 3.0;
+    }
+
+    public void scoreInfo() {
+        calcTotal();
+        calcAverage();
+
+        if(this.total == 0 || this.average == 0) {
+            System.out.println(this.name + "은 점수가 입력되지 않았습니다.");
+            System.out.println("--------------------------------------------------");
+            return;
+        }
+
+        System.out.printf("이름: %s\n국어: %d점\n영어: %d점\n수학: %d점\n총점: %d점\n평균: %.2f점\n",
+                this.name, this.korean, this.english, this.math, this.total, this.average);
+        System.out.println("--------------------------------------------------");
+    }
 }
--- src/oop/obj_arr/ScoreMain.java
+++ src/oop/obj_arr/ScoreMain.java
@@ -1,5 +1,8 @@
 package oop.obj_arr;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ScoreMain {
     public static void main(String[] args) {
         /*
@@ -19,6 +22,51 @@ public class ScoreMain {
           모두 null로 가득 차 있습니다. (null.scoreInfo() -> 에러)
          */
 
+        Score[] scores = new Score[100];
+        Scanner sc = new Scanner(System.in);
+
+        int i = 0;
+        while(true) {
+            // while(scores[scores.length-1] == null)
+            // while(i != scores.length)
+            if (i > scores.length - 1) {
+                System.out.println("배열 공간이 가득 차 있어서 데이터를 추가하지 못합니다.");
+                break;
+            }
+
+            System.out.print("\n이름을 입력하세요: ");
+            String name = sc.next();
+            if(name.equals("그만")) {
+                System.out.println("성적 추가를 그만둡니다.");
+                break;
+            }
+
+            
+            
+            System.out.print("\n국어 점수를 입력하세요: ");
+            int kr = sc.nextInt();
+
+
+            System.out.print("\n영어 점수를 입력하세요: ");
+            int en = sc.nextInt();
+
+
+            System.out.print("\n수학 점수를 입력하세요: ");
+            int mt = sc.nextInt();
+
+            scores[i] = new Score(name, kr, en, mt);
+            i++;
+            System.out.println("---------------------------------------------------------");
+        }
+
+        System.out.println("============================================================");
+//        System.out.println(Arrays.toString(scores));
+
+        for(int j=0; scores[j] != null && j<scores.length; j++) {
+            scores[j].scoreInfo();
+        }
+
+        sc.close();
 
 
 
