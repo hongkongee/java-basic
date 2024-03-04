2e3734a91d8b0dc1c2e98a480cee4b32d3879e1d
jeongwon <tjtkdvl@gmail.com>
2024-03-04 17:49:59 +0900

    # Java API (Wrapper, Arrays)

--- src/etc/api/lang/wrapper/WrapperExample.java
+++ src/etc/api/lang/wrapper/WrapperExample.java
@@ -8,9 +8,12 @@ public class WrapperExample {
         char c = 'A';
         double d = 3.14;
 
-        // boxing:
+        // boxing: 기본 데이터 타입을 객체 타입으로 변환하는 과정. -> deprecated
+//        Integer v1 = new Integer(a);
+//        Boolean v2 = new Boolean(b);
 
-        // unboxing:
+        // unboxing: 객체 타입으로 포장된 값을 기본 데이터 타입으로 풀어내는 과정.
+//        int i = v1.intValue(); -> deprecated
 
         // autoboxing: 기본 타입을 자동으로 객체형으로 변환
         Integer v1 = a;
@@ -18,14 +21,12 @@ public class WrapperExample {
         Character v3 = c;
         Double v4 = d;
 
-        // autoboxing: 객체 포장을 기본형으로 자동 해제 가능
+        // autounboxing: 객체 포장을 기본형으로 자동 해제 가능
         int i = v1;
         double d2 = v4;
 
         // Autoboxing 이후에 wrapper 클래스는 문자열을 기본형으로
         // 변환하는 데 많이 사용합니다.
-
-
         int i1 = Integer.parseInt("351");
         double d3 = Double.parseDouble("3.14");
 
